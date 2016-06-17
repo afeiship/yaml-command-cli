b91bf87f62bb0edb923fcdf38676206b980e376b
afei <88603982@qq.com>
2016-06-17 16:03:14 +0800

    initial

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,10 +11,17 @@ module.exports = yeoman.Base.extend({
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'module_name',
+      message: 'Your module name?'
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'Your module descriptioin?'
+    }, {
+      type: 'input',
+      name: 'wiki_url',
+      message: 'Your module related wiki?'
     }];
 
     return this.prompt(prompts).then(function (props) {
@@ -22,14 +29,62 @@ module.exports = yeoman.Base.extend({
       this.props = props;
     }.bind(this));
   },
-
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this._writingGulpfile();
+    this._writingIndexHtml();
+    this._writingPackageJson();
+    this._writingReadme();
+    this._writingImages();
+    this._writingSass();
+    this._writingScrips();
+  },
+  _writingGulpfile: function () {
+    this.fs.copyTpl(
+      this.templatePath('gulpfile.js'),
+      this.destinationPath('gulpfile.js'),
+      this.props
     );
   },
-
+  _writingIndexHtml: function () {
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('index.html'),
+      this.props
+    );
+  },
+  _writingPackageJson: function () {
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      this.props
+    );
+  },
+  _writingReadme: function () {
+    this.fs.copyTpl(
+      this.templatePath('README.MD'),
+      this.destinationPath('README.MD'),
+      this.props
+    );
+  },
+  _writingImages: function () {
+    this.fs.copy(
+      this.templatePath('src/images/*'),
+      this.destinationPath('src/images/')
+    );
+  },
+  _writingSass: function () {
+    this.fs.copy(
+      this.templatePath('src/sass/template.scss'),
+      this.destinationPath('src/sass/' + this.props.module_name + '.scss')
+    );
+  },
+  _writingScrips: function () {
+    this.fs.copyTpl(
+      this.templatePath('src/scripts/template.js'),
+      this.destinationPath('src/scripts/' + this.props.module_name + '.js'),
+      this.props
+    );
+  },
   install: function () {
     this.installDependencies();
   }
---
+++ generators/app/templates/module-template/gulpfile.js
@@ -0,0 +1,91 @@
+(function () {
+
+  'use strict';
+  var gulp = require('gulp');
+  var exec = require('child_process').exec;
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del', 'imagemin-pngquant'],
+    rename: {
+      'imagemin-pngquant': 'pngquant'
+    }
+  });
+
+  var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+  };
+
+  /* ================module build scripts Start====================== */
+  gulp.task('images', function () {
+    return gulp.src('src/images/*')
+      .pipe($.imagemin({
+        progressive: true,
+        use: [$.pngquant()]
+      }))
+      .pipe(gulp.dest('dist/images'));
+  });
+
+  gulp.task('images-watch', function () {
+    return gulp
+      .watch('src/images/*', ['images']);
+  });
+
+
+  gulp.task('scripts', function () {
+    return gulp.src('src/scripts/*')
+      .pipe($.uglify())
+      .pipe($.rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('dist/scripts'));
+  });
+
+  gulp.task('scripts-watch', function () {
+    return gulp
+      .watch('src/scripts/*', ['scripts']);
+  });
+
+
+  gulp.task('styles', function () {
+    return gulp
+      .src('src/sass/*.scss')
+      .pipe($.sourcemaps.init())
+      .pipe($.sass(sassOptions).on('error', $.sass.logError))
+      .pipe($.sourcemaps.write())
+      .pipe($.autoprefixer())
+      .pipe(gulp.dest('dist/styles'));
+  });
+
+  gulp.task('styles-watch', function () {
+    return gulp
+      .watch('src/styles/*.scss', ['styles']);
+  });
+
+
+  gulp.task('clean', function () {
+    return $.del(['dist']);
+  });
+
+  /* ================module build scripts End====================== */
+
+
+  /*===Default build scripts===*/
+  gulp.task('default', ['clean'], function () {
+    gulp.start([
+      'images',
+      'scripts',
+      'styles',
+      'images-watch',
+      'scripts-watch',
+      'styles-watch'
+    ]);
+  });
+
+
+  gulp.task('publish', function () {
+    exec('cd .. && gulp tgz --module=<%= module_name %>', function () {
+      console.info('[Success! Package path]:->', 'dist-module-packages/<%= module_name %>.tar.gz');
+    });
+  });
+
+}());
