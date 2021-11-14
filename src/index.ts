cb6bebfd0c23d227607580c4e5eb5344bab77594
afeiship <1290657123@qq.com>
2021-11-14 16:29:18 +0800

    feat: add single cmd

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,50 @@
-import { Command, flags } from "@oclif/command";
-import nx from "@jswork/next";
-import NxYamlConfiguration from "@jswork/next-yaml-configuration";
+import { Command, flags } from '@oclif/command';
+import { execSync } from 'child_process';
+import path from 'path';
+import nx from '@jswork/next';
+import NxYamlConfiguration from '@jswork/next-yaml-configuration';
 
 class YamlCommandCli extends Command {
-  static description = "describe the command here";
+  static description = 'describe the command here';
 
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({ char: "v" }),
-    help: flags.help({ char: "h" }),
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+    cmd: flags.string({
+      char: 'c',
+      description: 'Execute the cmd from list.'
+    }),
+    dryRun: flags.boolean({
+      char: 'd',
+      description: 'If show debug.'
+    })
   };
 
-  static args = [{ name: "config" }];
+  static args = [{ name: 'config' }];
+
+  private static getCmds(inCmds) {
+    return inCmds.join(' && ');
+  }
 
   private conf;
 
+  get commands() {
+    const cfg = this.conf.gets();
+    return cfg.commands;
+  }
+
   async run() {
     const { args, flags } = this.parse(YamlCommandCli);
-    this.conf = new NxYamlConfiguration({ path: args.config });
-    const cfg = this.conf.gets();
-    console.log(cfg);
-
+    if (!flags.cmd) return;
+    const cfgPath = args.config || path.join(process.cwd(), '.ycc.yml');
+    this.conf = new NxYamlConfiguration({ path: cfgPath });
+    const cmd = YamlCommandCli.getCmds(this.commands[flags.cmd!]);
+    if (flags.dryRun) {
+      console.log('config:', cfgPath);
+      console.log('command:', cmd);
+    } else {
+      console.log(execSync(cmd, { shell: '/bin/bash', encoding: 'utf8' }).trim());
+    }
   }
 }
 
